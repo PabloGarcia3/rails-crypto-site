ddd4751937846067de1985308de44cad9a423cbb
Pablo Garcia <pg3development@gmail.com>
2018-08-19 15:25:47 +0000

    Added Devise

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,33 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "ETH", "XRP", "ADA", "XLM", "STEEM",]
+    
+  end
+  
+  def about
+  end
+  
+  def lookup
+    require 'net/http'
+    require 'json'
+    
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin = JSON.parse(@response)
+    @symbol = params[:sym]
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+    
+    if @symbol == ""
+      @symbol = "Please enter an coin name"
+    end
+  end
+end
