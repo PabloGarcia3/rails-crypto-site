e5c8ad4e89d8a50e923aa181d62289090861a3d5
Pablo Garcia <pg3development@gmail.com>
2018-08-20 17:06:49 +0000

    styled devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # devise_for :installs
+   devise_for :users
+
   get 'home/about'
   get 'home/lookup'
   post "/home/lookup" => 'home/lookup'
-  root 'home#index'
+  root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
